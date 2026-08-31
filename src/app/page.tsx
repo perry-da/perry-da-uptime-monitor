f6c8445e2fbec4f3a9db7e7ba3da2968ea65b8fb
perry-da <perry@1000numbersaday.com>
2026-08-31 18:21:25 -0400

    add src/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function LandingPage() {
+  return (
+    <main>
+      <h1>Uptime Monitor</h1>
+      <p>Add a URL, get alerted before your customers notice.</p>
+      <a href="/signup">Sign up</a>
+      <a href="/login">Log in</a>
+    </main>
+  );
+}
